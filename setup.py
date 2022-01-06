df46d7952ba91756ea75056fa415aab073e32b99
atharva-naik <atharvanaik2018@gmail.com>
2022-01-07 01:45:59 +0530

    setup tools first draft

--- setup.py
+++ setup.py
@@ -1,2 +1,30 @@
 # setup.py
-import os
+from setuptools import setup
+
+README = open("README.md", "r").read()
+print(README)
+setup(
+    name="fig_dash",
+    version="0.0.1",
+    packages=["fig_dash"],
+    package_dir={"": 'fig_dash'},
+    url="https://github.com/atharva-naik/fig-dash",
+    license="GPLv3+",
+    long_description=README,
+    author="Atharva Naik",
+    author_email="atharvanaik2018@gmail.com",
+    description="Browser and Dashboard to boost poductivity",
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Browser',
+        'Topic :: Dashboard',
+    ],
+)
